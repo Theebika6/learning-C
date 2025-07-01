1109068e10cb17dfe09b94523e128cc805d71831
Theebika Thiyagaraja Iyer <theebika6@hotmail.com>
2025-07-01 15:03:51 -0400

    created node

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+ struct Node {
+    int data;
+    struct Node* next;
+} ;
+
+//create a new node
+
+struct Node* create_node(int data){
+
+    return NULL;
+};
+
+int main(void)
+{
+
+    printf("Hello World");
+
+    return 0;
+}
